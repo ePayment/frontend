42a5c01d0f70adec5d947db8da164669ab254719
huynq@payflow.vn <huynq@payflow.vn>
2015-01-23 23:05:08 +0700

    Portal API

---
+++ ePayment.API/Portal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ePayment.API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Portal" in both code and config file together.
+    public class Portal : IPortal
+    {
+
+        public Response Process(Request request)
+        {
+            request.FunctionCode = request.FunctionCode.ToLower();
+            switch (request.FunctionCode)
+            {
+                case "register":
+                    break;
+                case "login":
+                    break;
+                case "change_password":
+                    break;
+                default:
+                    return new Response { ResponseCode = "96", ResponseMessage = "Tính năng chưa được hỗ trợ" };
+            }
+        }
+    }
+}
---
+++ ePayment.API/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePayment.API
+{
+    public class Response
+    {
+        public string ResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+
+        public bool IsError { get { return ResponseCode.Equals("00"); } }
+    }
+
+    public class Request
+    {
+        public string FunctionCode { get; set; }
+        public string RequestData { get; set; }
+    }
+}
